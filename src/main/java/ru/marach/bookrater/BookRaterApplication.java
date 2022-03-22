09b8a78399f87d091d53c5cdf618740d04010ec2
Dmitry Marach <marach.dv@modul.tech>
2022-03-22 21:12:20 +0300

    Init commit

---
+++ src/main/java/ru/marach/bookrater/BookRaterApplication.java
@@ -0,0 +1,13 @@
+package ru.marach.bookrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/marach/bookrater/BookRaterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.marach.bookrater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
