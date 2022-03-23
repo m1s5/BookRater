4a6be57b5f7a78d5e4722f91e85149ed22a98b95
Dmitry Marach <marach.dv@modul.tech>
2022-03-24 02:17:20 +0300

    changes GenerationType

--- src/main/java/ru/marach/bookrater/models/entities/comment/Comment.java
+++ src/main/java/ru/marach/bookrater/models/entities/comment/Comment.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 @Data
 public class Comment {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
     @JoinColumn(name = "BOOK_ID")
--- src/main/java/ru/marach/bookrater/models/entities/comment/commentHistory/CommentHistoryEntry.java
+++ src/main/java/ru/marach/bookrater/models/entities/comment/commentHistory/CommentHistoryEntry.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @Table(name = "COMMENT_HISTORY")
 public class CommentHistoryEntry {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
