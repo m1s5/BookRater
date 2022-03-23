fdd7b2fa363c6d6aaecc4119f2dcc83e1b11b461
Dmitry Marach <marach.dv@modul.tech>
2022-03-24 00:53:08 +0300

    adds errors

---
+++ src/main/java/ru/marach/bookrater/models/entities/book/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.marach.bookrater.models.entities.book;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class BookNotFoundException extends RuntimeException {
+}
